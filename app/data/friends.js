2ded1cf0a5ae9f94c71e1433a4bb2a29aecbae2a
davevish <vishvesh01@yahoo.com>
2017-11-17 09:59:55 -0500

    changes to js file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,24 +1,24 @@
 var friends = [
 	{
-		name: "Drake",
-		photo: "drakepicture.jpg",
-		scores: [5, 2, 2, 1, 1, 3, 3, 4, 3, 1]
+		"name": "Drake",
+		"photo": "drakepicture.jpg",
+		"scores": [5, 2, 2, 1, 1, 3, 3, 4, 3, 1]
 	},{
-		name: "Vish",
-		photo: "vishpicture.jpg",
-		scores: [2, 1, 2, 2, 3, 1, 2, 3, 5, 5]
+		"name": "Vish",
+		"photo": "vishpicture.jpg",
+		"scores": [2, 1, 2, 2, 3, 1, 2, 3, 5, 5]
 	},{
-		name: "Rushil",
-		photo: "rushilpicture.JPG",
-		scores: [5, 5, 2, 1, 2, 6, 2, 4, 3, 1]
+		"name": "Rushil",
+		"photo": "rushilpicture.JPG",
+		"scores": [5, 5, 2, 1, 2, 6, 2, 4, 3, 1]
 	},{
-		name: "Jay",
-		photo: "jaypicture.jpg",
-		scores: [2, 4, 1, 2, 3, 3, 3, 3, 1, 1]
+		"name": "Jay",
+		"photo": "jaypicture.jpg",
+		"scores": [2, 4, 1, 2, 3, 3, 3, 3, 1, 1]
 	},{
-		name: "Chris",
-		photo: "chirspicture.jpg",
-		scores: [1, 3, 3, 4, 3, 1, 2, 4, 1, 5]
+		"name": "Chris",
+		"photo": "chirspicture.jpg",
+		"scores": [1, 3, 3, 4, 3, 1, 2, 4, 1, 5]
 }];
 
 module.export = friends;
