3fcf581a22c318bfbd60dd14b504b3f6a80bcb3b
davevish <vishvesh01@yahoo.com>
2017-11-17 00:20:46 -0500

    changes to logic and added gitignore file

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,7 +7,26 @@ module.exports = function (app) {
 
 	app.post("/api/friends", function (req, res) {
 		var newUser = req.body;
+
+		var bestFriend = {};
+
+		var largestDifference = 40;
+
+		var smallesDifference;
+
+		for (var i=0; i < friendsList.length; i++) {
+			var difference = 0;
+			for (var j = 0; j < friendsList[i].scores.length; i++) {
+                difference += Math.abs(friendsList[i].scores[j] - newUser.scores[j]);
+            }
+            	if(difference < largestDifference){
+				smallesDifference = difference;
+				bestFriend = {};
+				bestFriend = friendsList[i];
+				}
+			}
+
 		friendsList.push(newUser);
 		res.json(newUser)
-	});
+	})
 };
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
 		res.sendFile(path.join(__dirname, "../public/survey.html"));
 	});
 
-	app.get("*", function (res, req) {
-		res.sendFile(path.join(__dirname, "../public/home.html"));
-	});
+	// app.get("*", function (res, req) {
+	// 	res.sendFile(path.join(__dirname, "../public/home.html"));
+	// });
 };
